4a1869096814e3313e5c370b5427829f29bb8337
Vivek r <ramdurgvivek630@gmail.com>
2025-05-28 23:41:18 +0530

    port no.

--- server.js
+++ server.js
@@ -263,6 +263,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Portfolio server running at http://localhost:${PORT}`);
 });
